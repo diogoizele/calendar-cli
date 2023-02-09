304b8d6df9d112b51207d91a55614f4573bb168f
diogoizele <diogogabrielizele@gmail.com>
2023-02-09 14:22:03 -0300

    fix(date): fix date not valid by ascii converter

--- date.h
+++ date.h
@@ -3,34 +3,29 @@
 
 #define WEEK_DAYS_QUANTITY 7
 
-
 const char WEEK_DATES[] = {'S', 'M', 'T', 'W', 'T', 'F', 'S'};
 int DAYS_PER_MONTH[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-void get_day(char now[], char p_day[3])
-{
+void get_day(char now[], char p_day[3]) {
   p_day[0] = now[0];
   p_day[1] = now[1];
   p_day[2] = now[2];
 }
 
-void get_month(char now[], char p_month[3])
-{
+void get_month(char now[], char p_month[3]) {
   p_month[0] = now[4];
   p_month[1] = now[5];
   p_month[2] = now[6];
 }
 
-int get_date(char now[])
-{
+int get_date(char now[]) {
   int current_day;
-  char stringfy_number[2] = {0, 0};
+  int stringfy_number[2] = {0, 0};
 
   stringfy_number[0] = ascii_to_int((int)now[8]);
   stringfy_number[1] = ascii_to_int((int)now[9]);
 
-  if (now[9] == ' ')
-    return ascii_to_int((int)now[8]);
+  if (now[9] == ' ') return ascii_to_int((int)now[8]);
 
   stringfy_number[0] = stringfy_number[0] * 10;
   current_day = stringfy_number[0] + stringfy_number[1];
@@ -38,14 +33,12 @@ int get_date(char now[])
   return current_day;
 }
 
-int get_year(char now[])
-{
+int get_year(char now[]) {
   int result, i, pot;
 
   result = pot = i = 0;
 
-  for (i = 0; i < 4; i++)
-  {
+  for (i = 0; i < 4; i++) {
     pot = ((int)pow((double)10, (double)3 - i));
 
     result += ascii_to_int((int)now[20 + i]) * pot;
@@ -54,8 +47,7 @@ int get_year(char now[])
   return result;
 }
 
-int is_leap_year(int year)
-{
+int is_leap_year(int year) {
 
   if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
     return 1;
@@ -63,58 +55,45 @@ int is_leap_year(int year)
     return 0;
 }
 
-int jan_first_date_start(int year)
-{
+int jan_first_date_start(int year) {
   int example_year = 2023;
-  int example_date = 0; // representa segunda
+  int example_date = 0;  // representa segunda
   int date, year_diff;
   int initial_value = 0;
   date = 0;
 
   year_diff = year - example_year;
 
-  if (year < example_year)
-  {
-    for (int new_year = example_year; new_year >= year; new_year--)
-    {
-      if ((new_year - 1) < year)
-        break;
+  if (year < example_year) {
+    for (int new_year = example_year; new_year >= year; new_year--) {
+      if ((new_year - 1) < year) break;
 
-      if (is_leap_year(new_year - 1))
-      {
+      if (is_leap_year(new_year - 1)) {
         if (date == 1)
           date = 6;
         else if ((date - 2) < 0)
           date = 5;
         else
           date -= 2;
-      }
-      else
-      {
+      } else {
         if ((date - 1) < 0)
           date = 6;
         else
           date--;
       }
     }
-  }
-  else
-  {
-    for (int i = 0; i < year_diff; i++)
-    {
+  } else {
+    for (int i = 0; i < year_diff; i++) {
       int new_year = example_year + i;
 
-      if (is_leap_year(new_year))
-      {
+      if (is_leap_year(new_year)) {
         if (date == 5)
           date = 0;
         else if ((date + 2) > 6)
           date = 1;
         else
           date += 2;
-      }
-      else
-      {
+      } else {
         if ((date + 1) > 6)
           date = 0;
         else
@@ -126,41 +105,33 @@ int jan_first_date_start(int year)
   return date;
 }
 
-int round_week_date(int week_date, int days_quantity)
-{
+int round_week_date(int week_date, int days_quantity) {
   int days_quantity_week_date = days_quantity - 28;
 
-  if (days_quantity == 28)
-  {
+  if (days_quantity == 28) {
     return week_date;
   }
 
-  if (days_quantity == 29)
-  {
+  if (days_quantity == 29) {
     return week_date + 1;
   }
 
-  if (week_date + days_quantity_week_date > 6)
-  {
+  if (week_date + days_quantity_week_date > 6) {
     return days_quantity + week_date - 35;
   }
 
   return week_date + days_quantity_week_date;
 }
 
-int first_day_of_month(int first_day_of_jan, int numeric_month, int year)
-{
+int first_day_of_month(int first_day_of_jan, int numeric_month, int year) {
   int start_week_day = first_day_of_jan;
 
-  if (is_leap_year(year))
-  {
+  if (is_leap_year(year)) {
     DAYS_PER_MONTH[1] = 29;
   }
 
-  for (int i = 0; i <= numeric_month; i++)
-  {
-    if (i >= numeric_month)
-      break;
+  for (int i = 0; i <= numeric_month; i++) {
+    if (i >= numeric_month) break;
 
     int days_quantity = DAYS_PER_MONTH[i];
 
@@ -170,45 +141,43 @@ int first_day_of_month(int first_day_of_jan, int numeric_month, int year)
   return start_week_day;
 }
 
-void print_month(int numeric_month)
-{
-  switch (numeric_month)
-  {
-  case 0:
-    printf("       January");
-    break;
-  case 1:
-    printf("       February");
-    break;
-  case 2:
-    printf("        March");
-    break;
-  case 3:
-    printf("        April");
-    break;
-  case 4:
-    printf("         May");
-    break;
-  case 5:
-    printf("        June");
-    break;
-  case 6:
-    printf("        July");
-    break;
-  case 7:
-    printf("       August");
-    break;
-  case 8:
-    printf("      September");
-    break;
-  case 9:
-    printf("       October");
-    break;
-  case 10:
-    printf("      November");
-    break;
-  case 11:
-    printf("      December");
-    break;
+void print_month(int numeric_month) {
+  switch (numeric_month) {
+    case 0:
+      printf("       January");
+      break;
+    case 1:
+      printf("       February");
+      break;
+    case 2:
+      printf("        March");
+      break;
+    case 3:
+      printf("        April");
+      break;
+    case 4:
+      printf("         May");
+      break;
+    case 5:
+      printf("        June");
+      break;
+    case 6:
+      printf("        July");
+      break;
+    case 7:
+      printf("       August");
+      break;
+    case 8:
+      printf("      September");
+      break;
+    case 9:
+      printf("       October");
+      break;
+    case 10:
+      printf("      November");
+      break;
+    case 11:
+      printf("      December");
+      break;
   }
 }
--- main.c
+++ main.c
@@ -1,18 +1,18 @@
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <time.h>
-#include "date.h"
 #include "color.h"
+#include "date.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
   char day[3];
   char month[3];
-  int date, year, first_day_of_jan, month_in_number, quantity_days_current_month, f_d_month;
+  int date, year, first_day_of_jan, month_in_number,
+      quantity_days_current_month, f_d_month;
   char sun, mon, tue, wed, thu, fri, sat;
- 
+
   time_t now;
   time(&now);
 
@@ -25,8 +25,7 @@ int main(int argc, char **argv)
   quantity_days_current_month = DAYS_PER_MONTH[month_in_number];
   f_d_month = first_day_of_month(first_day_of_jan, month_in_number, year);
 
-  if (is_leap_year(year))
-  {
+  if (is_leap_year(year)) {
     DAYS_PER_MONTH[1] = 29;
   }
 
@@ -36,11 +35,9 @@ int main(int argc, char **argv)
   printf(" %d", year);
 
   printf("\n\n");
-  for (int d = 0; d < WEEK_DAYS_QUANTITY; d++)
-  {
+  for (int d = 0; d < WEEK_DAYS_QUANTITY; d++) {
     paint(YELLOW);
-    if (d == 0 || d == 6)
-      paint(RED);
+    if (d == 0 || d == 6) paint(RED);
 
     printf("\e[1m %c  \e[m", WEEK_DATES[d]);
     reset();
@@ -49,20 +46,15 @@ int main(int argc, char **argv)
 
   int break_line = WEEK_DAYS_QUANTITY - f_d_month;
 
-  for (int i = 0; i < f_d_month; i++)
-    printf("    ");
+  for (int i = 0; i < f_d_month; i++) printf("    ");
 
-  for (int d = 0; d < quantity_days_current_month; d++)
-  {
+  for (int d = 0; d < quantity_days_current_month; d++) {
     int altD = d + 1;
 
-    if (date == altD)
-    {
-      paint(BLUE);
+    if (date == altD) {
+      paint(GREEN);
       printf("\e[1m");
-    }
-    else
-    {
+    } else {
       printf("\e[m");
     }
 
@@ -71,16 +63,13 @@ int main(int argc, char **argv)
     else
       printf("%d  ", altD);
 
-    if ((altD) % (break_line) == 0)
-      printf("\n");
+    if ((altD) % (break_line) == 0) printf("\n");
 
     if (break_line == 1)
       break_line = WEEK_DAYS_QUANTITY + 1;
-    else if (altD > break_line)
-    {
+    else if (altD > break_line) {
       break_line = WEEK_DAYS_QUANTITY + break_line;
-    }
-    else if (altD >= WEEK_DAYS_QUANTITY + break_line)
+    } else if (altD >= WEEK_DAYS_QUANTITY + break_line)
       break_line = WEEK_DAYS_QUANTITY;
 
     reset();
